1686773a8d77bd2a69a154fae6ac1632e66bda34
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-05-27 14:47:16 +0530

    style part

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,42 @@
 
 <main>
   <h1>Random Facts Generator</h1>
-  <p>fact</p>
-  <button>Generate Random Fact</button>
+  <p>{fact}</p>
+  <button on:click={getRandomFact}>Generate Random Fact</button>
 </main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 2rem;
+  }
+
+  h1 {
+    font-size: 2rem;
+    margin-bottom: 1rem;
+  }
+
+  p {
+    margin-bottom: 1rem;
+  }
+
+  button {
+    padding: 0.5rem 1rem;
+    font-size: 1rem;
+  }
+
+  @media screen and (max-width: 620px) {
+    h1 {
+      font-size: 1.5rem;
+    }
+
+    p {
+      font-size: 1rem;
+    }
+
+    button {
+      font-size: 0.75rem;
+    }
+  }
+
+</style>
